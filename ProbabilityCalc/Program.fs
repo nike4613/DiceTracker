a9f8b174c2e966dc9054a45b620ac4203e635222
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-08-30 17:39:14 -0500

    Reduce tested difficulty to 2

--- ProbabilityCalc/Program.fs
+++ ProbabilityCalc/Program.fs
@@ -31,12 +31,9 @@ let rolldiff count skill diff =
 
 let calcProb =
     seq {
-        (*yield d 12 |> toProb |> outputName "1d12"
-        yield (d 12 |> toProb) =. !>12 |> toProb |> outputName "1d12=12"
-        yield rollsingle !>3 |> toProb |> outputName "rollsingle 3"*)
         for attr in 1..8 do
             for skill in 0..5 do
-                yield rolldiff attr !>skill 3 |> toProb 
+                yield rolldiff attr !>skill 2 |> toProb 
                 |> outputName $"attr {attr} skill {skill}"
     }
 

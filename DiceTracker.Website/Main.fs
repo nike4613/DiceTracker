144f835cb27961ef249d1095a371e49399e09693
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-08-31 01:14:50 -0500

    Attempt to add a monaco editor element (it doesn't work)

--- DiceTracker.Website/Main.fs
+++ DiceTracker.Website/Main.fs
@@ -3,29 +3,38 @@
 open Elmish
 open Bolero
 open Bolero.Html
+open Microsoft.JSInterop
 
 type Model =
     {
-        x: string
-    }
-
-let initModel =
-    {
-        x = ""
+        monaco: Monaco.Model
     }
 
 type Message =
     | Ping
+    | MonacoMessage of Monaco.Message
+    
+let initModel =
+    {
+        monaco = { editor = None ; value = "" }
+    }, Cmd.none
 
 let update message model =
     match message with
-    | Ping -> model
+    | Ping -> model, Cmd.none
+    | MonacoMessage msg -> 
+        let (mdl, cmd) = Monaco.update msg model.monaco
+        { model with monaco = mdl }, Cmd.map MonacoMessage cmd
 
 let view model dispatch =
-    text "Hello, world!"
+    div [attr.``class`` "main"] [
+        ecomp<Monaco.Editor,_,_> [] model.monaco (MonacoMessage >> dispatch)
+        div [attr.id "controls"] []
+        div [attr.id "results"] []
+    ]
 
-type MyApp() =
+type Application() =
     inherit ProgramComponent<Model, Message>()
 
     override this.Program =
-        Program.mkSimple (fun _ -> initModel) update view
+        Program.mkProgram (fun _ -> initModel) update view
---
+++ DiceTracker.Website/MonacoEditor.fs
@@ -0,0 +1,49 @@
+﻿
+module DiceTracker.Website.Monaco
+
+open Elmish
+open Bolero
+open Bolero.Html
+open Microsoft.JSInterop
+open Microsoft.AspNetCore.Components
+
+type Model =
+    {
+        editor: IJSObjectReference option
+        value: string
+    }
+    
+let getEditorValue (editor: IJSObjectReference) = editor.InvokeAsync<string>("getValue", []).AsTask() |> Async.AwaitTask
+let setEditorValue (editor: IJSObjectReference) (value: string) = editor.InvokeVoidAsync("setValue", [value]).AsTask() |> Async.AwaitTask
+    
+type Message =
+    | CreateEditor of Editor
+    | EditorCreated of IJSObjectReference
+
+and Editor() =
+    inherit ElmishComponent<Model, Message>()
+    
+    let elemRef = HtmlRef()
+
+    [<Inject>]
+    member val JSRuntime = Unchecked.defaultof<IJSRuntime> with get, set
+
+    override _.ShouldRender(oldModel, newModel) =
+        oldModel.value <> newModel.value || Option.isNone newModel.editor
+
+    override this.View model dispatch =
+        match model.editor with
+        | Some editor -> setEditorValue editor model.value |> ignore
+        | None -> CreateEditor this |> dispatch
+
+        div [ attr.ref elemRef ] []
+        
+let update message model =
+    match message with
+    | CreateEditor editor ->
+        let js = editor.JSRuntime
+        let cmd =
+            Cmd.OfTask.perform (fun args -> js.InvokeAsync("monacoCreate", args).AsTask())
+                [| s; "// this is the default text"; "fsharp" |] EditorCreated
+        model, cmd
+    | EditorCreated ref -> { model with editor = Some ref }, Cmd.none

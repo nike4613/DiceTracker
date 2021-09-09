3ad934f92e39dfede7d205260554816e2317df5e
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-09-08 21:00:43 -0500

    Add plot visualizations for results

--- ProbabilityCalc/Program.fs
+++ ProbabilityCalc/Program.fs
@@ -2,6 +2,7 @@
 module Dice
 
 open DiceTracker
+open XPlot.Plotly
 
 let rollsingle skill =
     prob {
@@ -32,20 +33,45 @@ let rolldiff count skill diff =
 
 let result =
     seq {
-        //yield output !>3
-        yield output (rollsingle !>5 |> toProb)
-        (*yield output (roll !>5 3)
         for attr in 1..8 do
             for skill in 0..5 do
                 yield rolldiff attr !>skill 2 |> toProb 
-                |> outputName $"attr {attr} skill {skill}"*)
+                |> outputName $"attr {attr} skill {skill}"
     }
 
 let rec printMapFull map = Map.iter (printfn "%s %A") map
 
+let stackedLayout = Layout(barmode = "stack")
+
 [<EntryPoint>]
 let main argv =
-    printfn ""
     let results = result |> Processing.processMany
     printMapFull results
+
+    let bars = 
+        results
+        |> Map.toSeq
+        |> Seq.map (fun (a, b) -> b |> Map.toSeq |> Seq.map (fun (b, c) -> a, b, c))
+        |> Seq.concat
+        |> Seq.fold (fun m (n, i, v) -> Map.change i (fun v2 -> (n,v)::(Option.defaultValue [] v2) |> Some) m) Map.empty
+        |> Map.map (fun _ v -> List.rev v)
+
+    let traces =
+        bars
+        |> Map.toSeq
+        |> Seq.map (fun (i, list) ->
+            Bar(
+                x = (list |> List.map (fun (n, _) -> n)),
+                y = (list |> List.map (fun (_, r) -> float r)),
+                name = string i
+            ))
+        |> Seq.toList
+
+    let chart =
+        traces
+        |> Chart.Plot
+        |> Chart.WithLayout stackedLayout
+
+    chart.Show()
+
     0 // return an integer exit code

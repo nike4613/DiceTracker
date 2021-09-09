206defde7489a190389867739116f5d82637e822
Anairkoen Schno <aaron.cirr.com@gmail.com>
2021-09-08 22:26:30 -0500

    Update test program to be easier to tweak

--- ProbabilityCalc/Program.fs
+++ ProbabilityCalc/Program.fs
@@ -4,15 +4,20 @@ module Dice
 open DiceTracker
 open XPlot.Plotly
 
+[<Literal>]
+let dicePool = 12
+[<Literal>]
+let target = 9
+
 let rollsingle skill =
     prob {
-        let! roll = d 12
+        let! roll = d dicePool
         let! modified = roll + (Arg 0)
-        return! condb (roll =. !>12)
+        return! condb (roll =. !>dicePool)
             (Literal true)
             (condb (roll =. !>1)
                 (Literal false)
-                (modified >. !>9))
+                (modified >. !>target))
     } |> funcnb "rollsingle" [skill]
 
 let roll skill count =
@@ -25,18 +30,22 @@ let rolldiff count skill diff =
     prob {
         let pityDice =
             if diff > count then
-                seq { for _ in 1..(diff - count) -> d 12 =. !>12 }
+                seq { for _ in 1..(diff - count) -> d dicePool =. !>dicePool }
                 |> Seq.map toProb |> Seq.reduce (+)
             else !>0
         return! ((roll (Arg 0) count) + pityDice >=. !>diff)
     } |> funcnb "rolldiff" [skill]
 
+let rolld22 = ((d 10) + (d 2) - !>1) + (cond ((d 2) =. !>2) !>11 !>0)
+
 let result =
     seq {
+        //yield output rolld22
         for attr in 1..8 do
             for skill in 0..5 do
-                yield rolldiff attr !>skill 2 |> toProb 
-                |> outputName $"attr {attr} skill {skill}"
+                for diff in 1..4 do
+                    yield rolldiff attr !>skill diff |> toProb 
+                    |> outputName $"diff {diff} attr {attr} skill {skill}"
     }
 
 let rec printMapFull map = Map.iter (printfn "%s %A") map
@@ -55,9 +64,6 @@ let main argv =
         |> Seq.concat
         |> Seq.fold (fun m (n, i, v) -> Map.change i (fun v2 -> (n,v)::(Option.defaultValue [] v2) |> Some) m) Map.empty
         |> Map.map (fun _ v -> List.rev v)
-
-    let traces =
-        bars
         |> Map.toSeq
         |> Seq.map (fun (i, list) ->
             Bar(
@@ -68,7 +74,7 @@ let main argv =
         |> Seq.toList
 
     let chart =
-        traces
+        bars
         |> Chart.Plot
         |> Chart.WithLayout stackedLayout
 
